643f7f9d81aeb72149d7e5d69d21dec7e5c0ce04
DhrishDJain <jaindhrish12@gmail.com>
2023-10-19 22:04:31 +0530

    working with blank filling feature

--- code.py
+++ code.py
@@ -16,6 +16,7 @@ class gui(Tk):
         global inco
         canvas=Canvas(background="#DE9E46",height=900)
         l=[]
+        index_loc=[]
         p=0
         d=int(len(w)/2)
         #logic to keep word in middle of window
@@ -25,18 +26,27 @@ class gui(Tk):
         st=540-sub
         for i in range(len(w)):
             l.append("_")
-            canvas.create_text(st+p,100,text=l[i],fill="#1c404c",font=('typewriter 50 bold'))
+            xcord=st+p
+            underscore=canvas.create_text(xcord,100+10,text=l[i],fill="#1c404c",font=('typewriter 50 bold'))
+            index_loc.append(xcord)
             p+=64
+        def blanks(i,user):
+            l[i]=str(user)
+            underscore=canvas.create_text(index_loc[i],100+10,text=l[i].upper(),fill="#1c404c",font=('typewriter 40 bold'))
+            index_loc.remove(index_loc[i])
+            # underscore.itemconfig(text=l[i])
+
         canvas.create_text(X_cord,Y_cord,text=text,fill=f"#1c404c",font=f"typewriter {s} bold")
         canvas.create_text(510,50,text="HANGMAN GAME",fill="#1c404c",font="gabriola 40 bold")
         canvas.create_text(830,20,text="CHANCES LEFT : ",fill="#1c404c",font="gabriola 33")
-        canvas.create_text(510,185,text="ENTER YOUR PRIDICTION ",fill="#1c404c",font="gabriola 33 bold")
+        canvas.create_text(510,185+100,text="ENTER YOUR PRIDICTION ",fill="#1c404c",font="gabriola 40 bold")
         inco=Entry(canvas,width=25,highlightcolor="#1c404c",font=("gabriola",17),highlightthickness=4,background="#DE9E46",highlightbackground="#1c404c",relief=RAISED)
-        inco.place(x=390,y=221)
+        inco.place(x=390,y=221+100)
         def take(event):
             global z
             
-            user=inco.get().lower()
+            user=inco.get()
+            # user.lower()
             inco.delete(0,END) 
             def clr(i):
                 # global z
@@ -51,8 +61,9 @@ class gui(Tk):
                         clr(z)
                     except:
                         pass
-                    z=canvas.create_text(300,300,text="YOU PRIDICTED A LETTER",fill=f"#1c404c",font=f"typewriter 12 bold")
+                    z=canvas.create_text(510,180+15,text="YOU PRIDICTED A LETTER",fill=f"#1c404c",font=f"gabriola 30 bold")
                     print(letter)
+                    blanks(i,user)
                     break
                 elif user==w:
                     letter.clear()
@@ -60,7 +71,7 @@ class gui(Tk):
                         clr(z)
                     except:
                         pass
-                    z=canvas.create_text(300,300,text="YOU PRIDICTED ENTIRE WORD",fill=f"#1c404c",font=f"typewriter 12 bold")
+                    z=canvas.create_text(510,180+15,text="YOU PRIDICTED ENTIRE WORD",fill=f"#1c404c",font=f"gabriola 30 bold")
                     print(letter)
                     break
 
@@ -69,13 +80,14 @@ class gui(Tk):
                          clr(z)
                     except:
                         pass
-                    z=canvas.create_text(300,300,text="You pridicted letter not match",fill=f"#1c404c",font=f"typewriter 12 bold")
-                    break
+                    z=canvas.create_text(510,180+15,text="You pridicted letter/word not match",fill=f"#1c404c",font=f"gabriola 30 bold")
+                    # break
+                    continue
         
             # self.CANVAS(user)
         butt=Button(canvas,text="PRIDICT",relief=RAISED,width=16,height=0,bg="#1c404c",font="gabriola 24 bold",foreground="#DE9E46",activebackground="#1c404c",justify=CENTER)
         butt.bind("<Button-1>",take)   
-        butt.place(x=391,y=274)
+        butt.place(x=391,y=274+100)
         canvas.pack(fill=BOTH)
         
 
