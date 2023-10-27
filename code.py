093ef221f1782370e0c9da0e394d9342caceee2d
DhrishDJain <jaindhrish12@gmail.com>
2023-10-27 23:09:40 +0530

    added hint with self destroying button

--- code.py
+++ code.py
@@ -25,15 +25,34 @@ class gui(Tk):
             global canvas
             self.clr(tkf)
             tkf=canvas.create_text(972,20,text=f"{chances}",fill="#1c404c",font="typewriter 27 bold")
- 
+
+    
+    def hint(self,e):
+        global canvas
+        global w    
+        print(w)
+        dic={"apple":"red","mango":"yellow","cherry":"red","peach":"peach","kiwi":"brown","grape":"green",'orange':"orange",'strawberry':"red"}
+        dic_keys=list(dic.keys())
+        dic_values=list(dic.values())
+        for i in range(len(dic_keys)):
+            if w==dic_keys[i]:
+                hint=canvas.create_text(350+30,560,text=f"the colour of the fruit is {dic_values[i]}".upper(),fill="#1c404c",font="gabriola 40 bold")
+                break
+        hint_butt.destroy()
+    
+
+
     #var to stop one extra calling of change function from inside of take funtion (declared ahead)
     global counter
     counter=0
-
+    global canvas
     #function to create canvas
     def CANVAS(self,chances,w,letter):
         global canvas
         global inco
+        global tkf
+        global hint_butt
+       
         canvas=Canvas(background="#DE9E46",height=900)
         l=[]
         index_loc=[] #list holding cordinate of _
@@ -43,6 +62,7 @@ class gui(Tk):
         para=len(w)/2
         sub=para*64
         st=540-sub
+
         #loop for printing intial _
         for i in range(len(w)):
             l.append("_")
@@ -50,6 +70,7 @@ class gui(Tk):
             underscore=canvas.create_text(xcord,100+10,text=l[i],fill="#1c404c",font=('typewriter 50 bold'))
             index_loc.append(xcord)
             p+=64
+
         #function placing correct pridicted letter to its correct postion in word
         def blank_filling(i,user):
             l[i]=str(user)
@@ -59,11 +80,14 @@ class gui(Tk):
                 index_loc.remove(index_loc[i])
             except:
                 pass
+
         #creating and placeing stationary canvas text 
         canvas.create_text(510,50,text="HANGMAN GAME",fill="#1c404c",font="gabriola 40 bold")
         tkf=canvas.create_text(972,20,text=f"{chances}",fill="#1c404c",font="typewriter 27 bold")
         canvas.create_text(830,20,text="CHANCES LEFT : ",fill="#1c404c",font="gabriola 33")
         canvas.create_text(510,185+100,text="ENTER YOUR PRIDICTION ",fill="#1c404c",font="gabriola 40 bold")
+        canvas.create_text(70,510,text="hint:".upper(),fill="#1c404c",font="gabriola 40 bold")
+       
 
         inco=Entry(canvas,width=25,highlightcolor="#1c404c",font=("gabriola",17),highlightthickness=4,background="#DE9E46",highlightbackground="#1c404c",relief=RAISED)
         inco.place(x=390,y=221+100)
@@ -71,7 +95,10 @@ class gui(Tk):
         def take_input(e):
             global z
             global chances
+            global canvas
             global counter
+        
+            
             user=inco.get()
             inco.delete(0,END)#calling inbuilt function that clear entries in entry width when called
             chances-=1
@@ -123,14 +150,14 @@ class gui(Tk):
 
             if counter==0:
                 self.change(chances)
-            
-        hint=Button(canvas,text="HINT",relief=RAISED,width=16,height=0,bg="#1c404c",font="gabriola 24 bold",foreground="#DE9E46",activebackground="#1c404c",justify=CENTER)
-        hint.bind("<Button-1>",self.hint)  
-        hint.place(x=391-100,y=274+100)
         butt=Button(canvas,text="PRIDICT",relief=RAISED,width=16,height=0,bg="#1c404c",font="gabriola 24 bold",foreground="#DE9E46",activebackground="#1c404c",justify=CENTER)
         butt.bind("<Button-1>",take_input)
         self.bind('<Return>',take_input)  
         butt.place(x=391,y=274+100)
+            
+        hint_butt=Button(canvas,text="HINT",relief=RAISED,width=10,height=0,bg="#1c404c",font="gabriola 24 bold",foreground="#DE9E46",activebackground="#1c404c",justify=CENTER)
+        hint_butt.bind("<Button-1>",self.hint)  
+        hint_butt.place(x=800,y=500)       
         canvas.pack(fill=BOTH)
     
     
@@ -140,6 +167,7 @@ class gui(Tk):
         global fru
         global chances
         global letter
+        global w
         fru=["apple","mango","cherry","peach","kiwi","grape",'orange','strawberry']
         w=random.choice(fru)
         chances=len(w)+4
